8cac9723e07e06f1b74302144eec06a2b0b6ba94
Jan Kratochvil <kratochvil.jan@outlook.com>
2022-03-13 13:07:10 +0100

    Laxer password rules for easier signup

--- lib/models/slices/auth/auth_validation_service.dart
+++ lib/models/slices/auth/auth_validation_service.dart
@@ -1,13 +1,4 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:tuple/tuple.dart';
-import 'package:collection/collection.dart';
-
-final List<Tuple2<RegExp, String>> passwordRegexRules = [
-  Tuple2(RegExp(r"\d+"), "At least one digit required."),
-  Tuple2(RegExp(r"[a-z]+"), "At least one lowercase letter required."),
-  Tuple2(RegExp(r"[A-Z]+"), "At least one uppercase letter required."),
-  Tuple2(RegExp(r"\W"), "At least one special character required."),
-];
 
 final emailValidationRule = RegExp(
     r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
@@ -32,12 +23,10 @@ class AuthValidationService {
       return 'Password cannot be empty.';
     }
 
-    if (value!.length < 6) {
-      return 'Password must have at least 6 characters';
+    if (value!.length < 5) {
+      return 'Password must have at least 5 characters';
     }
 
-    return passwordRegexRules
-        .firstWhereOrNull((x) => !x.item1.hasMatch(value))
-        ?.item2;
+    return null;
   }
 }

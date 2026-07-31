b41e09daf29a12c9dccaa473854c2068853114e0
Jan Kratochvil <kratochvil.jan@outlook.com>
2026-07-31 17:57:11 +0200

    Deploying to gh-pages from @ jankratochvilcz/kotobaten-monorepo@6cefdd0b0ea32954f50d99cbec65258c83726d0d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2461804164" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "217018212" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
